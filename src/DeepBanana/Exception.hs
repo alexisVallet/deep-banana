2240417c373b0bb966d147b2ad4c63d69d5e0851
Alexis Vallet <alexis.vallet@gmail.com>
2016-02-23 04:42:56 +0900

    GADT definition for Coproduct. Better Show instance.

--- src/DeepBanana/Exception.hs
+++ src/DeepBanana/Exception.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies, DataKinds, ImplicitParams #-}
+{-# LANGUAGE GADTs, TypeFamilies, DataKinds, ImplicitParams, StandaloneDeriving #-}
 module DeepBanana.Exception (
     WithStack
   , withStack
@@ -14,9 +14,9 @@ import DeepBanana.Prelude
 import Data.Typeable
 import GHC.Stack
 
-type family Coproduct l where
-  Coproduct '[a] = a
-  Coproduct (e ': l) = Either e (Coproduct l)
+data Coproduct (l :: [*]) where
+  Left' :: a -> Coproduct (a ': l)
+  Right' :: Coproduct l -> Coproduct (a ': l)
 
 class Variant t e where
   setVariant :: e -> t
@@ -26,17 +26,32 @@ instance Variant a a where
   setVariant = id
   getVariant = Just
 
-instance (Variant b e) => Variant (Either a b) e where
-  setVariant e = Right $ setVariant e
-  getVariant (Left _) = Nothing
-  getVariant (Right b) = getVariant b
+instance (Variant (Coproduct b) e) => Variant (Coproduct (a ': b)) e where
+  setVariant e = Right' $ setVariant e
+  getVariant (Left' _) = Nothing
+  getVariant (Right' b) = getVariant b
 
-instance {-# OVERLAPPING #-} Variant (Either a b) a where
-  setVariant a = Left a
-  getVariant (Left a) = Just a
-  getVariant (Right _) = Nothing
+instance {-# OVERLAPPING #-} Variant (Coproduct (a ': b)) a where
+  setVariant a = Left' a
+  getVariant (Left' a) = Just a
+  getVariant (Right' _) = Nothing
 
-instance (Show a, Show b, Typeable a, Typeable b) => Exception (Either a b)
+instance {-# OVERLAPPING #-} (Show a) => Show (Coproduct '[a]) where
+  show (Left' a) = show a
+
+instance (Show a, Show (Coproduct b)) => Show (Coproduct (a ': b)) where
+  show (Left' a) = show a
+  show (Right' b) = show b
+
+deriving instance (Typeable a) => Typeable (Coproduct '[a])
+deriving instance (Typeable a, Typeable (Coproduct b)) => Typeable (Coproduct (a ': b))
+
+deriving instance Typeable '[]
+deriving instance (Typeable a, Typeable b) => Typeable (a ': b)
+
+instance (Show a, Typeable a) => Exception (Coproduct '[a])
+instance (Show a, Show (Coproduct b), Typeable a, Typeable b, Typeable (Coproduct b))
+         => Exception (Coproduct (a ': b))
 
 throwVariant :: (MonadError t m,  Variant t e) => e -> m a
 throwVariant = throwError . setVariant
